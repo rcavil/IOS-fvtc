bc83862fe466726e0f6f11b04f91b112e8ad4494
rcavil <Ron@Ronald-Cavils-MacBook-Pro.local>
2014-01-14 20:16:51 -0600

    Initial Commit

---
+++ Unit 1/Assignment_01/Assignment_01/Assignment_01/HelloWorldViewController.m
@@ -0,0 +1,29 @@
+//
+//  HelloWorldViewController.m
+//  Assignment_01
+//
+//  Created by Ron on 1/14/14.
+//  Copyright (c) 2014 edu.self. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Unit 1/Assignment_01/Assignment_01/Assignment_01Tests/Assignment_01Tests.m
@@ -0,0 +1,34 @@
+//
+//  Assignment_01Tests.m
+//  Assignment_01Tests
+//
+//  Created by Ron on 1/14/14.
+//  Copyright (c) 2014 edu.self. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Assignment_01Tests : XCTestCase
+
+@end
+
+@implementation Assignment_01Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
