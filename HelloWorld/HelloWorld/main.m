5eec101652b61635e77bafa907499dfd5553feb8
rcavil <Ron@Ronald-Cavils-MacBook-Pro.local>
2014-01-14 19:37:58 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Ron on 1/14/14.
+//  Copyright (c) 2014 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloWorldAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloWorldAppDelegate class]));
+    }
+}
---
+++ Unit 1/HelloWorld/HelloWorld/HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Ron on 1/14/14.
+//  Copyright (c) 2014 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
