2db3e2376a91237a90b06b1b7deace249367ed16
Ron Cavil <cavil.ron@gmail.com>
2014-04-14 20:28:56 -0500

    Check in initial changes

---
+++ Final Project/MyMapFinder/MyMapFinder/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  MyMapFinder
+//
+//  Created by Ron Cavil on 4/14/14.
+//  Copyright (c) 2014 Ron Cavil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface ViewController : UIViewController
+<MKMapViewDelegate>
+
+//@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+- (IBAction)zoom:(id)sender;
+- (IBAction)type:(id)sender;
+@end
---
+++ Final Project/MyMapFinder/MyMapFinder/ViewController.m
@@ -0,0 +1,58 @@
+//
+//  ViewController.m
+//  MyMapFinder
+//
+//  Created by Ron Cavil on 4/14/14.
+//  Copyright (c) 2014 Ron Cavil. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <MapKit/MapKit.h>
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _mapView.showsUserLocation = YES;
+    _mapView.delegate = self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)zoom:(id)sender
+{
+    MKUserLocation *userLocation = _mapView.userLocation;
+    MKCoordinateRegion region =
+    MKCoordinateRegionMakeWithDistance (
+                                        userLocation.location.coordinate, 20000, 20000);
+    [_mapView setRegion:region animated:NO];
+}
+
+- (IBAction)type:(id)sender
+{
+    if (_mapView.mapType == MKMapTypeStandard)
+        _mapView.mapType = MKMapTypeSatellite;
+    else
+        _mapView.mapType = MKMapTypeStandard;
+}
+
+- (void)mapView:(MKMapView *)mapView
+didUpdateUserLocation:
+(MKUserLocation *)userLocation
+{
+    _mapView.centerCoordinate =
+    userLocation.location.coordinate;
+}
+@end
+
+
